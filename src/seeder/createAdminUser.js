0c670a64e55f1ff7ae2f724eb2ba0352146d95e2
AnupJP <anupp@thirdrocktechkno.com>
2025-01-28 01:15:59 +0530

    Seeder for Add Admin User

---
+++ src/seeder/createAdminUser.js
@@ -0,0 +1,50 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = require('../models/userModel');
+
+const adminUser = {
+    username: 'Admin',
+    email: 'panchalanup2572@gmail.com',
+    password: 'Admin@123', // You should change this password
+    mobile_no: '9999999999', // Optional: Change as needed
+    role: 'admin'
+}
+
+const createAdminUser = async () => {
+    try {
+        
+         // Test the connection to the database
+        //  await sequelize.authenticate();
+         console.log('Database connection established successfully.');
+
+        // Check if admin already exists
+        const existingAdmin = await User.findOne({
+            where: {
+                email: adminUser.email,
+                role: adminUser.role
+            }
+        });
+
+        if (existingAdmin) {
+            console.log('Admin user already exists!');
+            return;
+        }
+
+        // Create admin user
+         await User.create(adminUser);
+
+        console.log('Admin user created successfully!');
+        console.log(`Email: ${adminUser.email}`);
+        console.log(`Password: ${adminUser.password}`);
+
+    } catch (error) {
+        console.error('Error creating admin user:', error.message);
+    } finally {
+        // Close the database connection
+        process.exit();
+    }
+};
+
+// Execute the function
+createAdminUser();
